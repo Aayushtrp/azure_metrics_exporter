ed9518e91f28e765212c68427e496fdb14efe8ad
Aayushtrp <70259513+Aayushtrp@users.noreply.github.com>
2021-06-09 01:34:00 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/RobustPerception/azure_metrics_exporter/config"
+	"github.com/Aayushtrp/azure_metrics_exporter/config"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
